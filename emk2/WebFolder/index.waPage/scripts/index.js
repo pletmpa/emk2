d9d9cc3f872fb8dc5827f52addb3d8ced582d74d
gk <gk@mdvs.de>
2013-02-12 17:50:53 +0100

    Array Geschlecht und ko

--- emk2/WebFolder/index.waPage/scripts/index.js
+++ emk2/WebFolder/index.waPage/scripts/index.js
@@ -2,23 +2,51 @@
 WAF.onAfterInit = function onAfterInit() {// @lock
 
 // @region namespaceDeclaration// @startlock
+	var login1 = {};	// @login
+	var documentEvent = {};	// @document
 	var menuItem2 = {};	// @menuItem
 	var menuItem1 = {};	// @menuItem
 // @endregion// @endlock
 
 // eventHandlers// @lock
 
+	login1.logout = function login1_logout (event)// @startlock
+	{// @endlock
+		$$('menuBar1').hide();
+	};// @lock
+
+	login1.login = function login1_login (event)// @startlock
+	{// @endlock
+		$$('menuBar1').show();
+	};// @lock
+
+	documentEvent.onLoad = function documentEvent_onLoad (event)// @startlock
+	{// @endlock
+		
+		kommArray = [art];
+		kommArray.push({art: 'Telefon'});
+		kommArray.push({art: 'Mobile'});
+		kommArray.push({art: 'Fax'});
+		kommArray.push({art: 'Telefon geschäftlich'});
+		kommArray.push({art: 'Telefon sonstiges'});
+		WAF.sources.kommArray.sync();
+	};// @lock
+
 	menuItem2.click = function menuItem2_click (event)// @startlock
 	{// @endlock
-		$$('component1').loadComponent('./../Components/organisationen.waComponent');
+		
+		$$('component1').loadComponent('/views/organisation.waComponent');
 	};// @lock
 
 	menuItem1.click = function menuItem1_click (event)// @startlock
 	{// @endlock
-		$$('component1').loadComponent('./Components/personen.waComponent');
+		$$('component1').loadComponent('/views/main_personen.waComponent');
 	};// @lock
 
 // @region eventManager// @startlock
+	WAF.addListener("login1", "logout", login1.logout, "WAF");
+	WAF.addListener("login1", "login", login1.login, "WAF");
+	WAF.addListener("document", "onLoad", documentEvent.onLoad, "WAF");
 	WAF.addListener("menuItem2", "click", menuItem2.click, "WAF");
 	WAF.addListener("menuItem1", "click", menuItem1.click, "WAF");
 // @endregion
---
+++ emk2/WebFolder/views/main_personen.waComponent/main_personen.js
@@ -0,0 +1,35 @@
+﻿
+(function Component (id) {// @lock
+
+// Add the code that needs to be shared between components here
+
+function constructor (id) {
+
+	// @region beginComponentDeclaration// @startlock
+	var $comp = this;
+	this.name = 'main_personen';
+	// @endregion// @endlock
+
+	this.load = function (data) {// @lock
+
+	// @region namespaceDeclaration// @startlock
+	var dataGrid1 = {};	// @dataGrid
+	// @endregion// @endlock
+
+	// eventHandlers// @lock
+
+	dataGrid1.onRowDblClick = function dataGrid1_onRowDblClick (event)// @startlock
+	{// @endlock
+		$$('component1_tabView1').selectTab(3);
+	};// @lock
+
+	// @region eventManager// @startlock
+	WAF.addListener(this.id + "_dataGrid1", "onRowDblClick", dataGrid1.onRowDblClick, "WAF");
+	// @endregion// @endlock
+
+	};// @lock
+
+
+}// @startlock
+return constructor;
+})();// @endlock
