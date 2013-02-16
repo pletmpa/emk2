5707639ea711ad7d8f5d255eaf79d8377541a005
gk <gk@mdvs.de>
2013-02-16 19:04:47 +0100

    spende in Buchhaltung

--- emk2/WebFolder/views/buchhaltung.waComponent/buchhaltung.js
+++ emk2/WebFolder/views/buchhaltung.waComponent/buchhaltung.js
@@ -13,11 +13,25 @@ function constructor (id) {
 	this.load = function (data) {// @lock
 
 	// @region namespaceDeclaration// @startlock
+	var spenderFilter = {};	// @textField
+	var nameFilter = {};	// @textField
 	// @endregion// @endlock
 
 	// eventHandlers// @lock
 
+	spenderFilter.keyup = function spenderFilter_keyup (event)// @startlock
+	{// @endlock
+		source.component1_person.query('name= :1', $$('component1_spenderFilter').getValue() + '*');
+	};// @lock
+
+	nameFilter.keyup = function nameFilter_keyup (event)// @startlock
+	{// @endlock
+		source.component1_person.query('name= :1', $$('component1_nameFilter').getValue() + '*');
+	};// @lock
+
 	// @region eventManager// @startlock
+	WAF.addListener(this.id + "_spenderFilter", "keyup", spenderFilter.keyup, "WAF");
+	WAF.addListener(this.id + "_nameFilter", "keyup", nameFilter.keyup, "WAF");
 	// @endregion// @endlock
 
 	};// @lock
